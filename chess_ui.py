580fd60fc1eb775ac7c2774a082e4c83e12edb7a
cablemanager <caolancochrane@gmail.com>
2023-06-15 17:53:39 +0100

    Added fucktonnes of game logic and started movement rule regulation

--- chess_ui.py
+++ chess_ui.py
@@ -1,3 +1,22 @@
+class LegalMove(object):
+
+    def __init__(self, origin, destination):
+
+        self.origin = origin
+        self.destination = destination
+
+    def origin(self):
+        
+        return self.origin
+
+    def destination(self):
+
+        return self.destination
+    
+    def __str__(self):
+
+        return self.origin + '-' + self.destination
+
 class Turn(object):
 
     def __init__(self, player="white", wking="e1", bking="e8"):
@@ -82,7 +101,7 @@ def createBoard():
         for letter in range(8):
             board[str(chr(letter + 97)) + str(number + 1)] = None
 
-    board["a1"] = Piece("white", "rook", "a1", False,   "\033[37m\u265c ")
+    board["e4"] = Piece("white", "rook", "a1", False,   "\033[37m\u265c ")
     board["b1"] = Piece("white", "knight", "b1", False, "\033[37m\u265e ")
     board["c1"] = Piece("white", "bishop", "c1", False, "\033[37m\u265d ")
     board["d1"] = Piece("white", "queen", "d1", False,  "\033[37m\u265b ")
@@ -117,7 +136,7 @@ def createBoard():
     board["f7"] = Piece("black", "pawn", "f7", False, "\033[30m\u265F ")
     board["g7"] = Piece("black", "pawn", "g7", False, "\033[30m\u265F ")
     board["h7"] = Piece("black", "pawn", "h7", False, "\033[30m\u265F ")
-  
+
     return board
 
 def boardUI(board):
@@ -229,12 +248,35 @@ def putsSidesKingInCheck(a, b, turnTracker, board):
     N = nAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
     RQ = rqAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
     P = pAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
-    bqAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
-    if not N and not RQ and not P:
+    BQ = bqAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
+    K = kAttacking(KingL, KingN, tempBoard, tempTurnTracker.Turn())
+
+    if not N and not RQ and not P and not BQ and not K:
         return False
     else:
         return True
     
+def kAttacking(L, N, board, turn):
+
+    N = int(N)
+
+    uu = L + str(N + 1)
+    ur = chr(ord(L) + 1) + str(N + 1)
+    ul = chr(ord(L) - 1) + str(N + 1)
+    rr = chr(ord(L) + 1) + str(N)
+    ll = chr(ord(L) - 1) + str(N)
+    dd = L + str(N - 1)
+    dr = chr(ord(L) + 1) + str(N - 1)
+    dl = chr(ord(L) - 1) + str(N - 1)
+
+    coords = [uu, ur, ul, rr, ll, dd, dr, dl]
+
+    for co in coords:
+        if co in board and board[co]:
+            if board[co].PC() == "king":
+               return True
+    return False
+
 def pAttacking(L, N, board, turn):
 
     N = int(N)
@@ -247,14 +289,14 @@ def pAttacking(L, N, board, turn):
         rightP = chr(ord(L) + 1) + str(N - 1)
 
     if turn == "white":
-        if leftP in board and board[leftP].PC() == "pawn" and board[leftP].COL() != "white":
+        if leftP in board and board[leftP] and board[leftP].PC() == "pawn" and board[leftP].COL() != "white":
             return True
-        if rightP in board and board[rightP].PC() == "pawn" and board[rightP].COL() != "white":
+        if rightP in board and board[rightP] and board[rightP].PC() == "pawn" and board[rightP].COL() != "white":
             return True
     else:
-        if leftP in board and board[leftP].PC() == "pawn" and board[leftP].COL() != "black":
+        if leftP in board and board[leftP] and board[leftP].PC() == "pawn" and board[leftP].COL() != "black":
             return True
-        if rightP in board and board[rightP].PC() == "pawn" and board[rightP].COL() != "black":
+        if rightP in board and board[rightP] and board[rightP].PC() == "pawn" and board[rightP].COL() != "black":
             return True
         
     return False
@@ -266,26 +308,49 @@ def bqAttacking(L, N, board, turn):
     N = int(N)
 
     inCheck = False
-    
-    for i in range(N, 9):
-        co = chr(ord(L) + i) + str(i + 1)
-        if (ord(L) + i) < ord("i"):
-            print(co)
-    
-    for i in range(N, 9):
-        co = chr(ord(L) - i) + str(i + 1)
-        if (ord(L) - i) > (ord("a") - 1):
-            print(co)
 
-    for i in range(1, N):
-        co = chr(ord(L) + i) + str(8 - (i))
-        if i > 0 and (ord(L) + i) < ord("i"):
-            print(co)
-    for i in range(1, N):
-        co = chr(ord(L) - i) + str(8 - (i))
-        if i > 0 and (ord(L) - i) >= ord("a"):
-            print(co)
+    # UP RIGHT
 
+    Aletters = [chr(i) for i in range(ord(L) + 1, ord("i"))]
+    for i in range(len(Aletters)):
+        if N + i + 1 <= 8:
+            if board[Aletters[i] + str(N + i + 1)]:
+                co = board[Aletters[i] + str(N + i + 1)]
+                if co.COL() != turn and (co.PC() == "bishop" or co.PC() == "queen"):
+                    return True
+                else:
+                    break
+
+    #UP LEFT
+
+    Bletters = [chr(i) for i in range(ord("a"), ord(L))]
+
+    for i in range(len(Bletters)):
+        if N + i + 1 <= 8:
+            if board[Bletters[::-1][i] + str(N + i + 1)]:
+                co = board[Bletters[::-1][i] + str(N + i + 1)]
+                if co.COL() != turn and (co.PC() == "bishop" or co.PC() == "queen"):
+                    return True
+                else:
+                    break
+
+    for i in range(len(Aletters) - 1):
+        if N - i - 1 >= 1:
+            if board[Aletters[i] + str(N - i - 1)]:
+                co = board[Aletters[i] + str(N - i - 1)]
+                if co.COL() != turn and (co.PC() == "bishop" or co.PC() == "queen"):
+                    return True
+                else:
+                    break
+
+    for i in range(len(Bletters) - 1):
+        if N - i - 1 >= 1:
+            if board[Bletters[::-1][i] + str(N - i - 1)]:
+                co = board[Bletters[::-1][i] + str(N - i - 1)]
+                if co.COL() != turn and (co.PC() == "bishop" or co.PC() == "queen"):
+                    return True
+                else:
+                    break
 
     return False
 
@@ -299,8 +364,6 @@ def rqAttacking(L, N, board, turn):
 
     for i in range(N + 1, 9):
         co = L + str(i)
-        # if board[co]:
-            # print(turn, co, board[co].PC(), board[co].COL())
         if board[co]:
             if board[co].COL() != turn and (board[co].PC() == "rook" or board[co].PC() == "queen"):
                 return True
@@ -310,8 +373,6 @@ def rqAttacking(L, N, board, turn):
             
     for i in range(N - 1, 0, -1):
         co = L + str(i)
-        # if board[co]:
-            # print(turn, co, board[co].PC(), board[co].COL())
         if board[co]:
             if board[co].COL() != turn and (board[co].PC() == "rook" or board[co].PC() == "queen"):
                 return True    
@@ -320,8 +381,6 @@ def rqAttacking(L, N, board, turn):
             
     for i in range(ord(L) + 1, 105):
         co = chr(i) + str(N)
-        # if board[co]:
-            # print(turn, co, board[co].PC(), board[co].COL())
         if board[co]:
             if board[co].COL() != turn and (board[co].PC() == "rook" or board[co].PC() == "queen"):
                 return True
@@ -330,8 +389,6 @@ def rqAttacking(L, N, board, turn):
             
     for i in range(ord(L) - 1, 97, -1):
         co = chr(i) + str(N)
-        # if board[co]:
-            # print(turn, co, board[co].PC(), board[co].COL())
         if board[co]:
             if board[co].COL() != turn and (board[co].PC() == "rook" or board[co].PC() == "queen"):
                 return True
@@ -356,20 +413,132 @@ def nAttacking(L, N, board, turn):
     posPos.append(chr(ord(L) - 2) + str(N + 1))
 
     for co in posPos:
-        if len(co) > 2 or int(co[1]) < 1 or int(co[1]) > 8:
+        if (len(co) > 2 or int(co[1]) < 1 or int(co[1]) > 8):
             posPos.pop(posPos.index(co))
 
     for co in posPos:
 
-        piece = board[co]
-       # if piece:
-       #     print(piece.PC(), turn)
+        if co in board:
+            piece = board[co]
 
-        if piece and piece.PC() == "knight" and piece.COL() != turn:
+            if piece and piece.PC() == "knight" and piece.COL() != turn:
 
-            return True
+                return True
 
 
     return False
 
-startGame()
+def rqMovement(L, N, board, turn):
+
+    rqLegal = []
+
+    N = int(N)
+
+    for i in range(N + 1, 9):
+        co = L + str(i)
+        if board[co]:
+            if board[co].COL() != turn:
+                rqLegal.append(LegalMove((L + str(N)), co))
+                break
+
+            elif board[co]:
+                break
+        else:
+                rqLegal.append(LegalMove((L + str(N)), co))
+            
+    for i in range(N - 1, 0, -1):
+        co = L + str(i)
+        if board[co]:
+            if board[co].COL() != turn:
+                rqLegal.append(LegalMove((L + str(N)), co))
+                break
+
+            elif board[co]:
+                break
+        else:
+                rqLegal.append(LegalMove((L + str(N)), co))
+            
+    for i in range(ord(L) + 1, 105):
+        co = chr(i) + str(N)
+        if board[co]:
+            if board[co].COL() != turn:
+                rqLegal.append(LegalMove((L + str(N)), co))
+                break
+
+            elif board[co]:
+                break
+        else:
+                rqLegal.append(LegalMove((L + str(N)), co))
+            
+    for i in range(ord(L) - 1, 97, -1):
+        co = chr(i) + str(N)
+        if board[co]:
+            if board[co].COL() != turn:
+                rqLegal.append(LegalMove((L + str(N)), co))
+                break
+
+            elif board[co]:
+                break
+        else:
+                rqLegal.append(LegalMove((L + str(N)), co))
+
+            
+    return rqLegal
+
+def nMovement(L, N, board, turn):
+
+    nLegal = []
+
+    posPos = []
+
+    N = int(N)
+
+    posPos.append(chr(ord(L) + 1) + str(N - 2))
+    posPos.append(chr(ord(L) + 1) + str(N + 2))
+    posPos.append(chr(ord(L) + 2) + str(N - 1))
+    posPos.append(chr(ord(L) + 2) + str(N + 1))
+    posPos.append(chr(ord(L) - 1) + str(N - 2))
+    posPos.append(chr(ord(L) - 1) + str(N + 2))
+    posPos.append(chr(ord(L) - 2) + str(N - 1))
+    posPos.append(chr(ord(L) - 2) + str(N + 1))
+
+    for co in posPos:
+
+        if co in board:
+            piece = board[co]
+            if (piece and piece.COL() != turn) or not piece:
+
+                move = LegalMove((L + str(N)), co)
+                nLegal.append(move)
+
+    return nLegal
+
+def checklegal(turn):
+
+    legalMoves = []
+
+    board = createBoard()
+    boardUI(board)
+    for square in board:
+        if board[square] and (board[square].PC() == "rook" or board[square].PC() == "queen") and board[square].COL() == turn:
+            [legalMoves.append(i) for i in rqMovement(square[0], square[1], board, turn)]
+
+        elif board[square] and board[square].PC() == "knight" and board[square].COL() == turn:
+            [legalMoves.append(i) for i in nMovement(square[0], square[1], board, turn)]
+
+        
+
+def test():
+
+    lN = []
+
+    board = createBoard()
+    boardUI(board)
+    for square in board:
+        if board[square] and board[square].PC() == "knight" and board[square].COL() == "white":
+            [lN.append(i) for i in nMovement(square[0], square[1], board, "white")]
+
+
+test()
+
+# startGame()
